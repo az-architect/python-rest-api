9cdae3013e5aef9e957d278fdf3de7a25e958a89
Ali <liaquat.ali@accenture.com>
2022-03-28 13:20:03 +0200

    changed return to type json

--- api.py
+++ api.py
@@ -6,7 +6,6 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+    return jsonify("About","Hello World!")
 
 app.run()
